c5918645e06ddec017332874825685f614cf1551
Игорь Наточий <igornatochiy96@mail.ru>
2022-07-21 20:51:37 +0600

    Добавьте файлы проекта.

---
+++ Задача12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Radius = Convert.ToDouble(Console.ReadLine());
+            double mx = Convert.ToDouble(Console.ReadLine());
+            double my = Convert.ToDouble(Console.ReadLine());
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            double y0 = Convert.ToDouble(Console.ReadLine());
+            double lenght = Rectangle.GetLenght(Radius);
+            double square = Rectangle.GetSquare(Radius);
+            double f = Rectangle.Getf(Radius, mx, my, x0, y0);
+            Console.WriteLine($"{lenght} \n{square} \n{f}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задача12/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача12
+{
+    class Rectangle
+    {
+            static public double GetLenght(double Radius)
+            {
+                return 2 * 3.14 * Radius;
+            }
+            static public double GetSquare(double Radius)
+            {
+                return 3.14 * (Radius * Radius);
+            }
+            static public double Getf(double Radius, double mx, double my, double x0, double y0)
+            {
+                return 2 * (x0 - mx) + 2 * (y0 - my);
+
+            }
+    }
+}
